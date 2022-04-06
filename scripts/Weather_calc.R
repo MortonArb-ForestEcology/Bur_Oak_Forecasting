b72b7e16a39db0143d514ef9bb4a4e7e896c131b
LucienFitzpatrick <lfitzpatrick@mortonarb.org>
2022-04-06 13:29:38 -0500

    Helper functions added to the repo

--- scripts/Weather_calc.R
+++ scripts/Weather_calc.R
@@ -17,25 +17,25 @@
 #
 #-----------------------------------------------------------------------------------------------------------------------------------#
 # Calculating the Tmean for the growing season of that year
-weather_calc <- function(met.all, g_start=1, g_end=60){
+weather_calc <- function(met.all, g_start=1, g_end=90){
   
   
   #Calculating mean temperature, growing degree days using 5C, and gorwing degree days using 0c
   met.all$TMEAN <- (met.all$tmax..deg.c. + met.all$tmin..deg.c.)/2
   met.all$GDD5 <- ifelse(met.all$TMEAN>5, met.all$TMEAN-5, 0)
   met.all$GDD0 <- ifelse(met.all$TMEAN>0, met.all$TMEAN, 0)
-  met.all$CDD5 <- ifelse(met.all$tmin..deg.c.<5, met.all$tmin..deg.c.-0, 0)
+  met.all$CDD0 <- ifelse(met.all$tmin..deg.c.<0, 0-met.all$tmin..deg.c., 0)
   
   #Creating empty columns to fill in the loop
   met.all$GDD5.cum <- NA
   met.all$GDD0.cum <- NA
   met.all$NCD <- NA
   met.all$PTTGDD.cum <- NA
-  met.all$CDD5.cum <- NA
+  met.all$CDD0.cum <- NA
   
   #Setting the beginning and end (using julian yday) of the growing season for our Growing season mean
   g_start <- 1
-  g_end <- 60
+  g_end <- 90
   
   
   # Calculate the cumulative growing degree days for each day/year
@@ -44,8 +44,8 @@ weather_calc <- function(met.all, g_start=1, g_end=60){
     dat.gtmean <- dat.tmp[(dat.tmp$yday>=g_start & dat.tmp$yday<=g_end), ]
     gtmean <- mean(dat.gtmean$TMEAN, na.rm = TRUE)
     dat.tmp$Date <- as.Date(paste(dat.tmp$year, dat.tmp$yday, sep="-"), format="%Y-%j")
-    gdd5.cum=0; gdd0.cum=0; pttgdd.cum=0; cdd5.cum=0;
-    d5.miss = 0; d0.miss=0; ptt.miss=0; c5.miss = 0;
+    gdd5.cum=0; gdd0.cum=0; pttgdd.cum=0; cdd0.cum=0;
+    d5.miss = 0; d0.miss=0; ptt.miss=0; c0.miss = 0;
     ncd = 0
     for(i in 1:nrow(dat.tmp)){
       if(is.na(dat.tmp$GDD5[i]) & d5.miss<=3){ 
@@ -64,12 +64,12 @@ weather_calc <- function(met.all, g_start=1, g_end=60){
         gdd0.cum <- gdd0.cum+dat.tmp$GDD0[i] 
       }
       
-      if(is.na(dat.tmp$CDD5[i]) & c5.miss<=3){
-        c5.miss <- c5.miss+1 # Let us miss up to 3 consecutive days
-        cdd5.cum <- cdd5.cum+0
+      if(is.na(dat.tmp$CDD0[i]) & c0.miss<=3){
+        c0.miss <- c0.miss+1 # Let us miss up to 3 consecutive days
+        cdd0.cum <- cdd0.cum+0
       } else {
-        c5.miss = 0 # reset to 0
-        cdd5.cum <- cdd5.cum+dat.tmp$CDD5[i] 
+        c0.miss = 0 # reset to 0
+        cdd0.cum <- cdd0.cum+dat.tmp$CDD0[i] 
       }
       
       if(!is.na(dat.tmp$TMEAN[i]) & dat.tmp$TMEAN[i] < 0){
@@ -86,7 +86,7 @@ weather_calc <- function(met.all, g_start=1, g_end=60){
       
       dat.tmp[i,"GDD5.cum"] <- gdd5.cum
       dat.tmp[i,"GDD0.cum"] <- gdd0.cum
-      dat.tmp[i,"CDD5.cum"] <- cdd5.cum
+      dat.tmp[i,"CDD0.cum"] <- cdd0.cum
       dat.tmp[i, "NCD"] <- ncd
       dat.tmp[i,"PTTGDD.cum"] <- pttgdd.cum
       dat.tmp[i, "GTmean"] <- gtmean
@@ -94,7 +94,7 @@ weather_calc <- function(met.all, g_start=1, g_end=60){
     
     met.all[met.all$year==YR, "GDD5.cum"] <- dat.tmp$GDD5.cum
     met.all[met.all$year==YR, "GDD0.cum"] <- dat.tmp$GDD0.cum
-    met.all[met.all$year==YR, "CDD5.cum"] <- dat.tmp$CDD5.cum
+    met.all[met.all$year==YR, "CDD0.cum"] <- dat.tmp$CDD0.cum
     met.all[met.all$year==YR, "NCD"] <- dat.tmp$NCD
     met.all[met.all$year==YR, "GTmean"] <- dat.tmp$GTmean
     met.all[met.all$year==YR, "PTTGDD.cum"] <- dat.tmp$PTTGDD.cum
