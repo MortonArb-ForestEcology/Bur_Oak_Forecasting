cfc43c83443a05cee7c7278f865d92f9c14c4c0c
LucienFitzpatrick <lfitzpatrick@mortonarb.org>
2022-04-06 13:28:52 -0500

    Final model ensemble

--- scripts/4_Model_Fitting.R
+++ scripts/4_Model_Fitting.R
@@ -1,10 +1,11 @@
 #----------------------------------------------------------------------------------------------------------------------------------#
 # Script by: Lucien Fitzpatrick
 # Project: Bur Oak Forecasting (Quercus Quest)
-# Purpose: This script downloads the NPN data and cleans them
-# Inputs: 
+# Purpose: This script takes our filtered NPN data and fits our different model paramters.
+# Inputs: Full_Bur_Obs.csv which contains all npn observations nearby source populations matched with weather metrics
+#         Daymet_clean_data.csv which contains yearly weather data for each site of observation
 # Outputs:
-# Notes: 
+# Notes: Current models are YDAY null model, GDD5, PTTGDD5, linear GTmean
 #-----------------------------------------------------------------------------------------------------------------------------------#
 library(rjags)
 library(coda)
@@ -17,232 +18,36 @@ dat.burst$loc <- as.numeric(factor(dat.burst$site_id))
 dat.burst$sp <- as.numeric(factor(dat.burst$species))
 summary(dat.burst)
 
-#Creating a data frame to check the model output
-Check <- data.frame()
-l <- 1
-#We are using bloom to determine the species becasue it is missing two species: fusiformis and laurifolia
-
-
-#Creating the row for this species information
-Check[l, "species"] <- "Quercus macrocarpa"
-Check[l, "bur.nObs"] <- nrow(dat.burst)
-
-
-SP.burst <- as.data.frame(table(dat.burst[, "state"]))
-colnames(SP.burst) <- c("State", "Freq")
-
-#Checking the mean
-Check[l, "mean.bur"] <- mean(dat.burst$GDD5.cum, na.rm = T)
-Check[l, "sd.bur"] <- sd(dat.burst$GDD5.cum, na.rm = T)
-
-hierarchical_regression <- "
-model{
-  for(k in 1:n){
-      mu[k] <- THRESH[st[k]]  
-      y[k] ~ dnorm(mu[k], sPrec)
-  }
-  
-  for(j in 1:nSt){
-    THRESH[j] ~ dnorm(Tprior, aPrec)
-  }
-  aPrec ~ dgamma(1, 1)
-  Tprior ~ dunif(1, 600)
-  sPrec ~ dgamma(.01, .01)
-
-}
-"
-
-bur.list <- list(y = dat.burst$GDD5.cum, n = length(dat.burst$GDD5.cum),
-                   st = as.numeric(factor(dat.burst$state)), nSt = length(unique(dat.burst$state)))
-
-
-#Setting the number of MCMC chains and their parameters
-nchain = 3
-inits <- list()
-rngs <- c(737, 874, 869)
-for(i in 1:nchain){
-  inits[[i]] <- list(.RNG.name = "base::Super-Duper",
-                     .RNG.seed = as.integer(rngs[i])
-  )
-}
-
-#---------------------------------------------------------#
-#This section actually runs the model and then provides ways to check the output and clean it
-#---------------------------------------------------------#
-bur.model   <- jags.model (file = textConnection(hierarchical_regression),
-                             data = bur.list,
-                             inits = inits,
-                             n.chains = 3)
-
-
-#Converting the ooutput into a workable format
-bur.out   <- coda.samples (model = bur.model,
-                             variable.names = c("THRESH", "aPrec"),
-                             n.iter = 700000)
-
-
-#Checking that convergence happened
-bud.con <- gelman.diag(bur.out)
-bud.con
-
-#Removing burnin before convergence occurred
-burnin = 690000                                ## determine convergence from GBR output
-bur.burn <- window(bur.out,start=burnin)  ## remove burn-in
-summary(bur.burn)
-
-#converting them into a dataframe 
-bur.df <- as.data.frame(as.matrix(bur.burn))
-
-#calculating sd from the precison
-bur.df$sd <- 1/sqrt(bur.df[,"aPrec"])
-
-summary(bur.df)
-
-bur.df$Species <- unique(dat.burst$species)
-
-names(bur.df)[grep("THRESH", names(bur.df))] <- paste(SP.burst$State)
-
-write.csv(bur.df, file.path("../data_processed/model_output", paste0("Quercus_Quest_TT_model_bur.csv")), row.names=F)
-
-
-
-#---------------------------------------------------#
-#Here we start with the variable thermal time model. THis is like GDDX instead of GDD5 or GDD0
-#---------------------------------------------------#
-dat.burst <- read.csv("../data_processed/Full_Bur_Obs.csv")
-dat.burst$pln <- as.numeric(factor(dat.burst$individual_id))
-dat.burst$loc <- as.numeric(factor(dat.burst$site_id))
-dat.burst$sp <- as.numeric(factor(dat.burst$species))
-summary(dat.burst)
-
-dat.burst <- dat.burst[dat.burst$state == "IL",]
-dat.burst <- dat.burst[1:5,]
-
 lat.calc <- read.csv("../data_processed/Daymet_clean_data.csv")
 
-burst.list <- as.list(dat.burst)
-for(i in 1:max(lengths(burst.list))){
-  yr <- burst.list[["year"]][i]
-  yday <- burst.list[["yday"]][i]
-  site <- burst.list[["site_id"]][i]
-  temp <- lat.calc[lat.calc$year == yr & lat.calc$site == site,]
-  burst.list[["temp.vec"]][i] <- list(temp$TMEAN)
-}
-
-start <- c(2)
-end <- c(365)
-
-for(i in 2:nrow(dat.burst)){
-  start[i] <- ((i-1)*365) + 2
-  end[i] <- (i*365)
-}
-
-hierarchical_regression <- "
-model{
-  for(k in 1:n){
-  
-      cs[k,1] <- ifelse(temp[(k*365)-364] < 5, 0, (temp[(k*365)-364]-5))
-      
-      for (t in start[k]:end[k]) {
-        new.tmp[k,t] <- ifelse(temp[t] < 5, 0, (temp[t]-5))
-
-        cs[k,(t-((k-1)*365))] <- cs[k, ((t-((k-1)*365)) - 1)] + new.tmp[k,t]
-      }
-      
-      mu[k] <- cs[k,] >= 200
-      y[k] ~ dnorm(mu[k], sPrec)
-  }
-  
- #for(j in 1:nSt){
- #   THRESH[j] ~ dnorm(Tprior, aPrec)
- #}
-
-  #aPrec ~ dgamma(.01, .01)
-  #Tprior ~ dunif(1, 1000)
-  sPrec ~ dgamma(.01, .01)
-
-}
-"
-
-bur.list <- list(y = burst.list[["yday"]], n = length(burst.list[["yday"]]), 
-                 temp = as.numeric(unlist(burst.list[["temp.vec"]])), start = as.numeric(start), end = as.numeric(end),
-                 st = as.numeric(factor(burst.list[["state"]])), nSt = length(unique(burst.list[["state"]])))
-
-
-#Setting the number of MCMC chains and their parameters
-nchain = 3
-inits <- list()
-rngs <- c(737, 874, 869)
-for(i in 1:nchain){
-  inits[[i]] <- list(.RNG.name = "base::Super-Duper",
-                     .RNG.seed = as.integer(rngs[i])
-  )
-}
-
-#---------------------------------------------------------#
-#This section actually runs the model and then provides ways to check the output and clean it
-#---------------------------------------------------------#
-bur.model   <- jags.model (file = textConnection(hierarchical_regression),
-                           data = bur.list,
-                           inits = inits,
-                           n.chains = 3)
-
-
-#Converting the ooutput into a workable format
-bur.out   <- coda.samples (model = bur.model,
-                           variable.names = c("cs"),
-                           n.iter = 7000)
-
-
-#Checking that convergence happened
-bud.con <- gelman.diag(bur.out)
-bud.con
-
-#Removing burnin before convergence occurred
-burnin = 6900                                ## determine convergence from GBR output
-bur.burn <- window(bur.out,start=burnin)  ## remove burn-in
-summary(bur.burn)
-
-#converting them into a dataframe 
-bur.df <- as.data.frame(as.matrix(bur.burn))
-
-#calculating sd from the precison
-bur.df$sd <- 1/sqrt(bur.df[,"aPrec"])
-
-summary(bur.df)
-
-bur.df$Species <- unique(dat.burst$species)
-
-names(bur.df)[grep("THRESH", names(bur.df))] <- paste(SP.burst$State)
-
-write.csv(bur.df, file.path("../data_processed/model_output", paste0("Quercus_Quest_VarTT_model_bur.csv")), row.names=F)
-
-
-
-
-
 #---------------------------------------------------#
-#Here we start with the GDD0 because it's the easiest to convert and Lucien was curious
-#Does not work yet
+#Here we start with the GDD5 thermal time model.
+#This model is fit using calculated GDD5.cum values (y = GDD5.cum on date of known observation)
+#This model fits an accumulation threshold (THRESH) for each source location
 #---------------------------------------------------#
-hierarchical_regression <- "
+
+YDAY_model <- "
 model{
+  #This is the data model where the y=YDAY is used to fit THRESH
   for(k in 1:n){
       mu[k] <- THRESH[st[k]]  
       y[k] ~ dnorm(mu[k], sPrec)
   }
   
+  #This is the process model and fits the accumulation threshold for each site
   for(j in 1:nSt){
-    THRESH[j] ~ dnorm(Tprior, aPrec)
+    THRESH[j] ~ dnorm(Tprior, aPrec)T(0, )
   }
-  aPrec ~ dgamma(1, 1)
-  Tprior ~ dunif(1, 600)
+  
+  #This is where our priors are defined
+  aPrec ~ dgamma(.01, .01)
+  Tprior ~ dunif(90,120)
   sPrec ~ dgamma(.01, .01)
 
 }
 "
 
-bur.list <- list(y = dat.burst$GDD0.cum, n = length(dat.burst$GDD0.cum),
+bur.list <- list(y = dat.burst$yday, n = length(dat.burst$yday),
                  st = as.numeric(factor(dat.burst$state)), nSt = length(unique(dat.burst$state)))
 
 
@@ -251,15 +56,14 @@ nchain = 3
 inits <- list()
 rngs <- c(737, 874, 869)
 for(i in 1:nchain){
-  inits[[i]] <- list(.RNG.name = "base::Super-Duper",
+  inits[[i]] <- list(aPrec = runif(1,1/5000,1/100), THRESH = runif(3,100,300), sPrec = runif(1,1/100,1/20),
+                     .RNG.name = "base::Super-Duper",
                      .RNG.seed = as.integer(rngs[i])
   )
 }
 
-#---------------------------------------------------------#
 #This section actually runs the model and then provides ways to check the output and clean it
-#---------------------------------------------------------#
-bur.model   <- jags.model (file = textConnection(hierarchical_regression),
+bur.model   <- jags.model (file = textConnection(YDAY_model),
                            data = bur.list,
                            inits = inits,
                            n.chains = 3)
@@ -290,6 +94,249 @@ summary(bur.df)
 
 bur.df$Species <- unique(dat.burst$species)
 
-names(bur.df)[grep("THRESH", names(bur.df))] <- paste(SP.burst$State)
+names(bur.df)[grep("THRESH", names(bur.df))] <- unique(dat.burst$state)
 
-write.csv(bur.df, file.path("../data_processed/model_output", paste0("Quercus_Quest_GDD0_model_bur.csv")), row.names=F)
+write.csv(bur.df, file.path("../data_processed/model_output", paste0("Quercus_Quest_YDAY_model_bur.csv")), row.names=F)
+
+
+#---------------------------------------------------#
+#Here we start with the GDD5 thermal time model.
+#This model is fit using calculated GDD5.cum values (y = GDD5.cum on date of known observation)
+#This model fits an accumulation threshold (THRESH) for each source location
+#---------------------------------------------------#
+
+GDD5_model <- "
+model{
+  #This is the data model where the y=GDD5.cum is used to fit THRESH
+  for(k in 1:n){
+      mu[k] <- THRESH[st[k]]  
+      y[k] ~ dnorm(mu[k], sPrec)
+  }
+  
+  #This is the process model and fits the accumulation threshold for each site
+  for(j in 1:nSt){
+    THRESH[j] ~ dnorm(Tprior, aPrec)T(0, )
+  }
+  
+  #This is where our priors are defined
+  aPrec ~ dgamma(.01, .01)
+  Tprior ~ dnorm(180, 50)
+  sPrec ~ dgamma(.01, .01)
+
+}
+"
+
+bur.list <- list(y = dat.burst$GDD5.cum, n = length(dat.burst$GDD5.cum),
+                 st = as.numeric(factor(dat.burst$state)), nSt = length(unique(dat.burst$state)))
+
+
+#Setting the number of MCMC chains and their parameters
+nchain = 3
+inits <- list()
+rngs <- c(737, 874, 869)
+for(i in 1:nchain){
+  inits[[i]] <- list(aPrec = runif(1,1/5000,1/100), THRESH = runif(3,100,300), sPrec = runif(1,1/100,1/20),
+                     .RNG.name = "base::Super-Duper",
+                     .RNG.seed = as.integer(rngs[i])
+  )
+}
+
+#This section actually runs the model and then provides ways to check the output and clean it
+bur.model   <- jags.model (file = textConnection(GDD5_model),
+                           data = bur.list,
+                           inits = inits,
+                           n.chains = 3)
+
+
+#Converting the ooutput into a workable format
+bur.out   <- coda.samples (model = bur.model,
+                           variable.names = c("THRESH", "aPrec"),
+                           n.iter = 700000)
+
+
+#Checking that convergence happened
+bud.con <- gelman.diag(bur.out)
+bud.con
+
+#Removing burnin before convergence occurred
+burnin = 690000                                ## determine convergence from GBR output
+bur.burn <- window(bur.out,start=burnin)  ## remove burn-in
+summary(bur.burn)
+
+#converting them into a dataframe 
+bur.df <- as.data.frame(as.matrix(bur.burn))
+
+#calculating sd from the precison
+bur.df$sd <- 1/sqrt(bur.df[,"aPrec"])
+
+summary(bur.df)
+
+bur.df$Species <- unique(dat.burst$species)
+
+names(bur.df)[grep("THRESH", names(bur.df))] <- unique(dat.burst$state)
+
+write.csv(bur.df, file.path("../data_processed/model_output", paste0("Quercus_Quest_GDD5_model_bur.csv")), row.names=F)
+
+
+#---------------------------------------------------#
+#Next we have the PTTGDD thermal time model.
+#This model is fit using calculated PTTGDD5.cum values (y = PTTGDD5.cum on date of known observation)
+#PTTGDD is GDD5 * (day length in hours/24)
+#This model fits an accumulation threshold (THRESH) for each source location
+#---------------------------------------------------#
+PTTGDD_model  <- "
+model{
+ #This is the data model where the y=PTTGDD5.cum is used to fit THRESH
+  for(k in 1:n){
+      mu[k] <- THRESH[st[k]]  
+      y[k] ~ dnorm(mu[k], sPrec)
+  }
+  
+  #This is the process model and fits the accumulation threshold for each site
+  for(j in 1:nSt){
+    THRESH[j] ~ dnorm(Tprior, aPrec)T(0, )
+  }
+  
+  #This is where our priors are defined
+  aPrec ~ dgamma(.01, .01)
+  Tprior ~ dnorm(80, 20)
+  sPrec ~ dgamma(.01, .01)
+
+}
+"
+
+bur.list <- list(y = dat.burst$PTTGDD.cum, n = length(dat.burst$PTTGDD.cum),
+                 st = as.numeric(factor(dat.burst$state)), nSt = length(unique(dat.burst$state)))
+
+
+#Setting the number of MCMC chains and their parameters
+nchain = 3
+inits <- list()
+rngs <- c(737, 874, 869)
+for(i in 1:nchain){
+  inits[[i]] <- list(aPrec = runif(1,1/5000,1/100), THRESH = runif(3,10,150), sPrec = runif(1,1/100,1/20),
+                     .RNG.name = "base::Super-Duper",
+                     .RNG.seed = as.integer(rngs[i])
+  )
+}
+
+#This section actually runs the model and then provides ways to check the output and clean it
+bur.model   <- jags.model (file = textConnection(PTTGDD_model),
+                           data = bur.list,
+                           inits = inits,
+                           n.chains = 3)
+
+
+#Converting the ooutput into a workable format
+bur.out   <- coda.samples (model = bur.model,
+                           variable.names = c("THRESH", "aPrec"),
+                           n.iter = 700000)
+
+
+#Checking that convergence happened
+bud.con <- gelman.diag(bur.out)
+bud.con
+
+#Removing burnin before convergence occurred
+burnin = 690000                                ## determine convergence from GBR output
+bur.burn <- window(bur.out,start=burnin)  ## remove burn-in
+summary(bur.burn)
+
+#converting them into a dataframe 
+bur.df <- as.data.frame(as.matrix(bur.burn))
+
+#calculating sd from the precison
+bur.df$sd <- 1/sqrt(bur.df[,"aPrec"])
+
+summary(bur.df)
+
+bur.df$Species <- unique(dat.burst$species)
+
+names(bur.df)[grep("THRESH", names(bur.df))] <- unique(dat.burst$state)
+
+write.csv(bur.df, file.path("../data_processed/model_output", paste0("Quercus_Quest_PTTGDD_model_bur.csv")), row.names=F)
+
+
+#---------------------------------------------------#
+#Here we start with the GDD5 thermal time model.
+#This model is fit using calculated GDD5.cum values (y = GDD5.cum on date of known observation)
+#This model fits an accumulation threshold (THRESH) for each source location
+#---------------------------------------------------#
+
+GTmean_model <- "
+model{
+  #This is the data model where the y=Yday is used to fit an intercept and slope
+  for(k in 1:n){
+      mu[k] <- int[st[k]] + slope[st[k]]*GTmean[k]  
+      y[k] ~ dnorm(mu[k], sPrec)
+  }
+  
+  #This is the process model and fits the accumulation threshold for each site
+  for(j in 1:nSt){
+    int[j] ~ dnorm(Tprior,iPrec)
+    slope[j] ~ dnorm(0,xPrec)
+  }
+  
+  #This is where our priors are defined
+  Tprior ~ dunif(90,121)
+  sPrec ~ dgamma(.01, .01)
+  iPrec ~ dgamma(.01, .01)
+  xPrec ~ dgamma(.01, .01)
+  
+}
+"
+
+bur.list <- list(y = dat.burst$yday, n = length(dat.burst$yday), 
+                 st = as.numeric(factor(dat.burst$state)), nSt = length(unique(dat.burst$state)),
+                 GTmean = dat.burst$GTmean)
+
+
+#Setting the number of MCMC chains and their parameters
+nchain = 3
+inits <- list()
+rngs <- c(737, 874, 869)
+for(i in 1:nchain){
+  inits[[i]] <- list(sPrec = runif(1,1/100,1/20),
+                     .RNG.name = "base::Super-Duper",
+                     .RNG.seed = as.integer(rngs[i])
+  )
+}
+
+#This section actually runs the model and then provides ways to check the output and clean it
+bur.model   <- jags.model (file = textConnection(GTmean_model),
+                           data = bur.list,
+                           inits = inits,
+                           n.chains = 3)
+
+
+#Converting the ooutput into a workable format
+bur.out   <- coda.samples (model = bur.model,
+                           variable.names = c("int", "slope", "iPrec", "xPrec"),
+                           n.iter = 700000)
+
+
+#Checking that convergence happened
+bud.con <- gelman.diag(bur.out)
+bud.con
+
+#Removing burnin before convergence occurred
+burnin = 690000                                ## determine convergence from GBR output
+bur.burn <- window(bur.out,start=burnin)  ## remove burn-in
+summary(bur.burn)
+
+#converting them into a dataframe 
+bur.df <- as.data.frame(as.matrix(bur.burn))
+
+names(bur.df)[grep("int", names(bur.df))] <- paste0("int", unique(dat.burst$state))
+names(bur.df)[grep("slope", names(bur.df))] <- paste0("slope", unique(dat.burst$state))
+
+#calculating sd from the precison
+bur.df$int.sd <- 1/sqrt(bur.df[,"iPrec"])
+bur.df$slope.sd <- 1/sqrt(bur.df[,"xPrec"])
+
+summary(bur.df)
+
+bur.df$Species <- unique(dat.burst$species)
+
+write.csv(bur.df, file.path("../data_processed/model_output", paste0("Quercus_Quest_GTmean_model_bur.csv")), row.names=F)
+
